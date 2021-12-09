65c254c81408fcda5665b6056e12ef367ed9412b
Jaime Colman <mjackerjaime@gmail.com>
2021-12-09 17:40:46 -0300

    Menu interface deleted.

--- Exe.cs
+++ Exe.cs
@@ -12,108 +12,11 @@ namespace ProC7
     {
         static int Main(string[] args)
         {
-            try
-            {
-            // item declaration area
-                Menu compras = new Menu("compras");
-                compras.Add("harina");
-                compras.Add("leche");
-                compras.Add("azucar");
-                //compras.ShowChoises();
-
-                Menu autos = new Menu("Autos");
-                autos.Add("Toyota");
-                autos.Add("BMW");
-                autos.Add("Audi");
-                //autos.ShowChoises();
-
-                Menu chapters = new Menu("Chapters");
-                chapters.Add("Chapter one.");
-                chapters.Add("Chapter two.");
-                chapters.Add("Chapter three.");
-
-                Menu languages = new Menu("Languages");
-                languages.Add("Spanish");
-                languages.Add("English");
-                languages.Add("Chinese");
-
-                chapters.AddSub(languages);
-
-                // menu of menus
-                Menu m = new Menu("Principal");
-                m.Add("Welcome.txt");
-                m.AddSub(chapters);
-                m.AddSub(compras);
-                m.AddSub(autos);
+            test.Main();
 
 
-                // testing to add value after sync with menu.
-                compras.Add("galleta");
-
-                // Menu choose option logic
-                List<Menu> contenedor = new List<Menu>();
-                contenedor.Add(m);
-                contenedor[0].ShowChoises();
-                Console.Write("\n\nChoose option>> ");
-                int r = UserChoise(); // manage  format exeptions
-                int level = 0;
-                while (true)
-                {
-                    if (contenedor[level].GetChoiseBool(r))
-                    {
-                        Console.Write("TEMP: intercambio next nodo.");
-                        contenedor.Add(contenedor[level].GetSubMenu(r));
-                        level++;
-                    }
-                    else if (r > 0 && r <= contenedor[level].GetChoiseCount())
-                    {
-                        Console.WriteLine("OPEN FILE: {0}\n\n Press enter to continue...", contenedor[level].GetChoiseName(r));
-                        Console.ReadLine();
-                    }
-                    else if (r == 0)
-                    {
-                        Console.Write("Going back!.");
-                        contenedor.RemoveAt(contenedor.Count - 1);
-                        level--;
-                    }
-
-                    Console.Clear();
-                    Console.Write("Level: {0}", level);
-                    if (level < 0)
-                    {
-                        System.Environment.Exit(0);
-                    }
-                    contenedor[level].ShowChoises();
-                    Console.Write("\n\nChoose option>> ");
-                    r = UserChoise();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Warning: {0}", e.Message);
-                Console.ReadLine();
-            }
-                return 0;
-        }
-
-        static void Line(string s)
-        {
-            Console.WriteLine("\n-----------------------\n{0}\n-----------------------\n", s);
-        }
-
-        static int UserChoise()
-        {
-            while (true)
-            {
-                try
-                {
-                    return Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException Fe)
-                {
-                    Console.WriteLine("Warning: {0}\nPlease try again...", Fe.Message);
-                }
-            }
+            Console.ReadLine();
+            return 0;
         }
     }
 }
---
+++ MJTest/MJEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProC7.MJTest
+{
+    class MJEnum
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Opciones.a);
+        }
+        enum Opciones
+        {
+            a = 100,
+            b = 200,
+            c = 300,
+        }
+    }
+}
+
